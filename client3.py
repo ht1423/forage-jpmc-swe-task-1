bab6cf342d181162cd541a1c3d81b9a5a46b9576
ht1423 <tharshita0215@gmail.com>
2024-09-02 04:04:56 +0530

    Update getDataPoint calculation

--- client3.py
+++ client3.py
@@ -31,29 +31,40 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Corrected calculation for price
     return stock, bid_price, ask_price, price
 
 
+
 def getRatio(price_a, price_b):
-    """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return float('inf')  # or handle as appropriate
+    return price_a / price_b
+
+
 
 
 # Main
 if __name__ == "__main__":
+    prices = {}  # Dictionary to store stock prices
+
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
-        """ ----------- Update to get the ratio --------------- """
+        # Update prices dictionary with data from getDataPoint
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price  # Store the price in the dictionary
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Check if we have prices for at least two stocks to calculate the ratio
+        if len(prices) >= 2:
+            # Get the list of stock prices
+            stock_prices = list(prices.values())
+            # Calculate the ratio between the first two stock prices
+            ratio = getRatio(stock_prices[0], stock_prices[1])
+            print("Ratio %s" % ratio)
